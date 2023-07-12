13761c9869014a2ad560e7ce78d6fa24ff9ecee9
unknown <albertkantwi@gmail.com>
2023-07-12 08:57:40 +0100

    Add class.js file with code for javascript classes

---
+++ class.js
@@ -0,0 +1,70 @@
+class BookCollection {
+    constructor() {
+      this.books = JSON.parse(localStorage.getItem('bookCollection')) || [];
+    }
+  
+    displayBooks() {
+      const bookListDiv = document.querySelector('.books-list');
+      bookListDiv.innerHTML = '';
+  
+      this.books.forEach((book, index) => {
+        const bookDiv = document.createElement('div');
+        bookDiv.classList.add('book-details');
+        bookDiv.innerHTML = `
+          <strong>${book.title}</strong> By: ${book.author}
+        `;
+  
+        const removeBtn = document.createElement('button');
+        removeBtn.classList.add('remove-btn');
+        removeBtn.textContent = 'Remove';
+        removeBtn.addEventListener('click', () => this.removeBook(index));
+
+        const hr = document.createElement('hr');
+  
+        bookDiv.appendChild(removeBtn);
+        bookDiv.appendChild(hr);
+        bookListDiv.appendChild(bookDiv);
+      });
+    }
+  
+    addBook(event) {
+      event.preventDefault();
+  
+      const titleInput = document.querySelector('.title');
+      const authorInput = document.querySelector('.author');
+  
+      const title = titleInput.value;
+      const author = authorInput.value;
+  
+      if (title && author) {
+        const newBook = {
+          title,
+          author,
+        };
+  
+        this.books.push(newBook);
+  
+        localStorage.setItem('bookCollection', JSON.stringify(this.books));
+  
+        titleInput.value = '';
+        authorInput.value = '';
+  
+        this.displayBooks();
+      }
+    }
+  
+    removeBook(index) {
+      this.books.splice(index, 1);
+  
+      localStorage.setItem('bookCollection', JSON.stringify(this.books));
+  
+      this.displayBooks();
+    }
+  }
+  
+  const bookCollection = new BookCollection();
+  
+  const addBookForm = document.querySelector('.addForm');
+  addBookForm.addEventListener('submit', (event) => bookCollection.addBook(event));
+  
+  bookCollection.displayBooks();
